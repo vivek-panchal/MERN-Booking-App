da60e2fda931f6fcb725dff9b854f0a9b26886cd
vivek-panchal <vivek.nsut@gmail.com>
2024-02-22 19:01:55 +0530

    cors error

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use('/uploads',express.static(__dirname+'/uploads'));
 
 app.use(cors({
     credentials:true,
-    origin:'http://localhost:5173'
+    origin:'https://hotelbazaar.vercel.app/'
 }))
 
 
